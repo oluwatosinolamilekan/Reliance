564adc655bca63f3c60e108ad56734429080f5be
Your Name <oluwatosin.olamilekan@54gene.com>
2019-11-07 15:07:24 +0100

    added response url for the Api Documentation

--- app/Controllers/AccountController.php
+++ app/Controllers/AccountController.php
@@ -10,6 +10,21 @@ class AccountController
 
     const ContractCode = 2957982769;
 
+    public function getHeaderAuthorization(Type $var = null)
+    {
+        return "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+
+    }
+    private function getTransactionUrl()
+    {
+        return $this->get('https://sandbox.monnify.com/api/v1/merchant/transactions/query');
+    }
+
+    private  function getRervserAccountDetails()
+    {
+       return $this->get('https://sandbox.monnify.com/api/v1/bank-transfer/reserved-accounts/reference12345');
+    }
+
     public function getAccount()
     {
         
