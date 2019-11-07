a770b509d18359da93fd9a5d65d741f40ed03745
Your Name <oluwatosin.olamilekan@54gene.com>
2019-11-07 15:41:27 +0100

    login view rechange

--- Views/index.php
+++ Views/index.php
@@ -9,35 +9,24 @@
 </head>
 <body>
 
-<div class="container">
-
-  <div class="row">
-    <div class="col-md-8 order-md-1">
-      <h4 class="mb-3">Login</h4>
-      <form class="needs-validation" novalidate>
-        <div class="row">
-          <div class="col-md-6 mb-3">
-            <label for="Email">Email</label>
-            <input type="email" class="form-control" id="email"  name="email" placeholder="" value="" required>
-        
-          </div>
-          <div class="col-md-6 mb-3">
-            <label for="Password">Last name</label>
-            <input type="password" class="form-control" id="password" name="password" placeholder="" value="" required>
-           
-          </div>
-
-          <div class="col-md-6 mb-3">
-            <label for="Password">Remember me (for 90 days)</label>
-            <input type="checkbox" class="form-control">
-           
-          </div>
-        </div>
-        <hr class="mb-4">
-        <button class="btn btn-primary btn-lg btn-block" type="submit">Submit</button>
-      </form>
-    </div>
+<div class="container jumbotron">
+  <h1 class="display-4">Login!</h1>
+  <hr class="my-4">
+  <div class="form-group col-md-5">
+    <label for="email">Email address:</label>
+    <input type="email" class="form-control" id="email" Placeholder="Email">
   </div>
+  <div class="form-group col-md-5">
+    <label for="pwd">Password:</label>
+    <input type="password" class="form-control" id="pwd" Placeholder="Password">
+  </div>
+  <div class="form-group form-check">
+    <label class="form-check-label">
+      <input class="form-check-input" type="checkbox"> 
+      Remember me (for 90 days)
+    </label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
 
 </body>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
